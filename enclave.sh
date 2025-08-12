eb90e5b8333e4ffb672c99a7dbb624e960204a71
ProCom bv <194399206+dev-procom@users.noreply.github.com>
2025-08-12 11:00:55 +0200

    Create enclave.sh

---
+++ enclave.sh
@@ -0,0 +1,4 @@
+git clone --branch ProCom-Sandbox --single-branch https://github.com/dev-procom/enclave-scripts.git
+cd enclave-scripts
+chmod +x EnclaveSetup.sh ConfigureEnclave.ps1
+sudo ./EnclaveSetup.sh
